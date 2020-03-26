096bf449f3792ea136122e7416a5139aea25dd9f
zhiyong <zhiyongtj@gmail.com>
2020-03-26 17:06:18 +0800

    搜索微信群内好友

---
+++ MyWechat_GroupFriends.py
@@ -0,0 +1,43 @@
+# coding: utf-8
+
+import itchat, datetime
+import pandas as pd
+from pandas import DataFrame
+from itchat.content import TEXT
+
+itchat.auto_login(hotReload=True)  # 不需要重复扫码登录
+
+qun_name = '12.26京东云（天津）项目路演天津'
+Wxm_list = []
+Ncm_list = []
+
+
+class WeChat(object):
+
+    def get_all_info_from_wechat(self):
+        # itchat.auto_login(enableCmdQR = False)
+        # 获取群
+        roomslist = itchat.get_chatrooms()
+        # 群名称
+        itchat.dump_login_status()  # 显示所有的群聊信息，默认是返回保存到通讯录中的群聊
+        myroom = itchat.search_chatrooms(name=qun_name)  # 群聊名称
+        gsq = itchat.update_chatroom(myroom[0]['UserName'], detailedMember=True)
+        with open('科技金融.txt', 'w', encoding='utf-8')as f:
+            f.write("交流群成员如下\n")
+            for i in gsq['MemberList']:
+                # f.write('微信名：' + i['NickName'] + '|群昵称：' + i['DisplayName'] + '\n')
+                # print('微信名：' + i['NickName'] + '|群昵称：' + i['DisplayName'] + '\n')
+                # f.close()
+
+                Wxm_list.append(i['NickName'])
+                Ncm_list.append(i['DisplayName'])
+
+        dic_wxq = {'微信名': Wxm_list, '群昵称': Ncm_list}
+
+        df = DataFrame(dic_wxq)
+        df.to_excel(qun_name +'.xlsx')
+
+
+if __name__ == '__main__':
+    obj = WeChat()
+    obj.get_all_info_from_wechat()
